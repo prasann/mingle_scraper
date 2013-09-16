b8ff545372ad79417552688cea6fdadb9c01ab49
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2013-09-16 12:39:10 +0200

    Grouped by internal systems involved

--- scraper.rb
+++ scraper.rb
@@ -20,9 +20,11 @@ class Mingle
   end
 
   def get_all_cards(page)
-    xml = RestClient.get "#{@protocol}://#{@user_name}:#{@password}@#{@api_url}/cards.xml?tree_name=Story%20Tree&page=#{page}"
+    p "Collecting Data from mingle for page : #{page}"
+    xml = RestClient.get "#{@protocol}://#{@user_name}:#{@password}@#{@api_url}/cards.xml?page=#{page}"
     #xml = RestClient.get 'http://prasanna.local:8000/cards.xml'
     xml_obj = Crack::XML.parse(xml)
+    p "Done with page : #{page}"
     xml_obj["cards"].collect { |card_xml| Card.new(card_xml, self) }
   end
 end
@@ -43,7 +45,7 @@ class Card
     release_card && release_card.name
   end
 
-  def property(name)
+  def property(name='Internal System(s) involved')
     match = self.properties.find { |prop| prop["name"].downcase == name.downcase }
     return match.nil? unless match
     if match["type_description"] =~ /card/i
@@ -61,7 +63,7 @@ end
 class Scraper
   def self.scrape
     mingle = Mingle.new
-    cards = (1..2).collect { |page| mingle.get_all_cards(page) }.flatten
+    cards = (1..5).collect { |page| mingle.get_all_cards(page) }.flatten
     html = ERB.new(File.read('result.html.erb')).result({:cards => cards}.instance_eval { binding })
     File.open("document.html", "w+") { |f| f.write(html) }
   end
